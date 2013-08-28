7974ed2c3744ee3178e327ae453c19ae20fc42a3
cianclarke <cian@cianclarke.com>
2013-08-28 14:57:22 -0400

    Typo in fix for test on build server

--- test/test_fhact.js
+++ test/test_fhact.js
@@ -57,7 +57,8 @@ module.exports = {
   'test $fh.call bad arguments url' : function(test, assert){
     $fh.call('fefe', {}, function(err, res){
       if (res){
-        assert.ok(res.stats === 503 && res.body.indexOf('Service Temporarily Unavailable')>-1);
+        assert.ok(res.status === 503);
+        assert.ok(res.body.indexOf('Service Temporarily Unavailable')>-1);
       }else{
         assert.ok(err, 'Err was not populated - err is ' + JSON.stringify(err) + ' and res is ' + JSON.stringify(res));
       }
