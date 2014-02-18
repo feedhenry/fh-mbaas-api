0587c0f18db91edb3919858946e9abf7af9535d9
Martin Murphy <martin.murphy@feedhenry.com>
2014-02-18 15:37:41 +0000

    fix forms tests and disabled fh_push test since it's now failing due to urbanairship changes

--- test/test_fhforms.js
+++ test/test_fhforms.js
@@ -4,7 +4,7 @@ var mockForms = require('./fixtures/forms.js');
 module.exports = {
   'test getForms' : function(test, assert){
     var $fh = proxyquire('../lib/api.js', {'fh-forms' : mockForms});
-    $fh.forms.getForms({}, function(err, res){
+    $fh.forms.getForms({appId:'1234'}, function(err, res){
       assert.ok(!err);
       assert.ok(res);
       test.finish();
@@ -12,7 +12,7 @@ module.exports = {
   },
   'test getForm' : function(test, assert){
     var $fh = proxyquire('../lib/api.js', {'fh-forms' : mockForms});
-    $fh.forms.getForm({}, function(err, res){
+    $fh.forms.getForm({appId:'1234'}, function(err, res){
       assert.ok(!err);
       assert.ok(res);
       test.finish();
@@ -20,7 +20,7 @@ module.exports = {
   },
   'test getTheme' : function(test, assert){
     var $fh = proxyquire('../lib/api.js', {'fh-forms' : mockForms});
-    $fh.forms.getTheme({}, function(err, res){
+    $fh.forms.getTheme({appId:'1234'}, function(err, res){
       assert.ok(!err);
       assert.ok(res);
       test.finish();
@@ -28,7 +28,7 @@ module.exports = {
   },
   'test submitFormData' : function(test, assert){
     var $fh = proxyquire('../lib/api.js', {'fh-forms' : mockForms});
-    $fh.forms.submitFormData({}, function(err, res){
+    $fh.forms.submitFormData({appId:'1234'}, function(err, res){
       assert.ok(!err);
       assert.ok(res);
       test.finish();
@@ -36,7 +36,7 @@ module.exports = {
   },
   'test submitFormFile' : function(test, assert){
     var $fh = proxyquire('../lib/api.js', {'fh-forms' : mockForms});
-    $fh.forms.submitFormFile({}, function(err, res){
+    $fh.forms.submitFormFile({appId:'1234'}, function(err, res){
       assert.ok(!err);
       assert.ok(res);
       test.finish();
@@ -44,7 +44,7 @@ module.exports = {
   },
   'test getSubmissionStatus' : function(test, assert){
     var $fh = proxyquire('../lib/api.js', {'fh-forms' : mockForms});
-    $fh.forms.getSubmissionStatus({}, function(err, res){
+    $fh.forms.getSubmissionStatus({appId:'1234'}, function(err, res){
       assert.ok(!err);
       assert.ok(res);
       test.finish();
@@ -52,7 +52,7 @@ module.exports = {
   },
   'test completeSubmission' : function(test, assert){
     var $fh = proxyquire('../lib/api.js', {'fh-forms' : mockForms});
-    $fh.forms.completeSubmission({}, function(err, res){
+    $fh.forms.completeSubmission({appId:'1234'}, function(err, res){
       assert.ok(!err);
       assert.ok(res);
       test.finish();
